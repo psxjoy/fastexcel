775fe437e30ae7944f0d2e3fae97eec7a46e5dd4
DeleiGuo <delei@apache.org>
2025-10-01 21:38:39 +0800

    feat: add Matomo analytics tracking script (#626)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -200,6 +200,29 @@ const config = {
         }
     }),
     themes: ['@docusaurus/theme-mermaid'],
+    headTags: [
+        {
+            tagName: 'script',
+            attributes: {
+                type: 'text/javascript',
+            },
+            innerHTML: `
+              var _paq = window._paq = window._paq || [];
+               /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(["setDoNotTrack", true]);
+              _paq.push(["disableCookies"]);
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://analytics.apache.org/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '83']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            `,
+        },
+    ],
     markdown: {
         format: 'md',
         mermaid: true,
